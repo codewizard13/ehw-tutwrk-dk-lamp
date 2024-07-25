cb343b2834552f8a06f71d0f58a326d71c248d65
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-25 05:46:58 -0500

    UPDATE main.md:  ADD vid 41 notes - error handling

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -5,31 +5,42 @@ if (! isset($_POST['submit'])) {
 } else {
   include_once 'dbh.inc.php';
 
-  $first = mysqli_real_escape_string($conn, $_POST['first']);
-  $last = mysqli_real_escape_string($conn, $_POST['last']);
-  $email = mysqli_real_escape_string($conn, $_POST['email']);
-  $uid = mysqli_real_escape_string($conn, $_POST['uid']);
-  $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+  // $first = mysqli_real_escape_string($conn, $_POST['first']);
+  // $last = mysqli_real_escape_string($conn, $_POST['last']);
+  // $email = mysqli_real_escape_string($conn, $_POST['email']);
+  // $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+  // $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+  $first = $_POST['first'];
+  $last = $_POST['last'];
+  $email = $_POST['email'];
+  $uid = $_POST['uid'];
+  $pwd = $_POST['pwd'];
 
   //Ensure the post variables aren't empty
   if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
     header("Location: ../index.php?signup=empty");
   } else {
-    // code
+    //Ensure is valid email
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      header("Location: ../index.php?signup=invalidemail");
+    } else {
+      echo "Sign up the user!";
+    }
   }
 
   
-  $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
-  $stmt = mysqli_stmt_init($conn);
+  // $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
+  // $stmt = mysqli_stmt_init($conn);
 
-  if (! mysqli_stmt_prepare($stmt, $sql)) {
-    echo "SQL error!<br>";
-  } else {
-    mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
-    mysqli_stmt_execute($stmt);
-  }
+  // if (! mysqli_stmt_prepare($stmt, $sql)) {
+  //   echo "SQL error!<br>";
+  // } else {
+  //   mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+  //   mysqli_stmt_execute($stmt);
+  // }
   
-  header("Location: ../index.php?signup=success");  
+  // header("Location: ../index.php?signup=success");  
 
 }
 
