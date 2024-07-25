60adf0f7b8ebc20db8678474d5fb0a88d5a82183
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-25 05:38:35 -0500

    UPDATE signup.inc.php:  ADD error handler for if post vars empty

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -10,10 +10,18 @@ if (! isset($_POST['submit'])) {
   $email = mysqli_real_escape_string($conn, $_POST['email']);
   $uid = mysqli_real_escape_string($conn, $_POST['uid']);
   $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+  //Ensure the post variables aren't empty
+  if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
+    header("Location: ../index.php?signup=empty");
+  } else {
+    // code
+  }
+
   
   $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
   $stmt = mysqli_stmt_init($conn);
-  
+
   if (! mysqli_stmt_prepare($stmt, $sql)) {
     echo "SQL error!<br>";
   } else {
